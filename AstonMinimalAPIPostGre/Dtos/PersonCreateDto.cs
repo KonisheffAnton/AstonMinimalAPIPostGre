3674d53c1f81bf27a7bb60df633c23d9c35395f2
Конышев Антон Иванович <konysheffanton@yandex.ru>
2023-01-10 23:37:53 +0300

    This

--- AstonMinimalAPIPostGre/Dtos/PersonCreateDto.cs
+++ AstonMinimalAPIPostGre/Dtos/PersonCreateDto.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Security.Policy;
+using System.Xml.Linq;
 
 namespace AstonMinimalAPIPostGre.Dtos
 {
     public class PersonCreateDto
     {
+       
+
         public int ItemId { get; set; }
         public string Name { get; set; }
         public string Homeworld { get; set; }
         public ICollection<int> FilmIds { get; set; }
         public int vehicleId { get; set; }
         public string Url { get; set; }
+
+        public PersonCreateDto(int itemId, string name, string homeworld, ICollection<int> filmIds, int vehicleId, string url)
+        {
+            ItemId = itemId;
+            Name = name;
+            Homeworld = homeworld;
+            FilmIds = filmIds;
+            this.vehicleId = vehicleId;
+            Url = url;
+        }
+
+        public PersonCreateDto()
+        {
+        }
     }
+   
+
 }
